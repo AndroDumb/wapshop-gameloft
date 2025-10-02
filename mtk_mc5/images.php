c9331f687af1fd58d43862c49d25efe667f27d87
AndroDumb <torreszabdiel29@gmail.com>
2025-10-02 12:08:53 -0400

    codigo mas completo

---
+++ mtk_mc5/images.php
@@ -0,0 +1,99 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gameloft Store</title>
+        <link rel="stylesheet" type="text/css" href="http://wapshop.gameloft.com/mtk_mc5/css/reset.css">
+        <link rel="stylesheet" type="text/css" href="http://wapshop.gameloft.com/mtk_mc5/css/style.css">
+		
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <script type="text/javascript" src="http://wapshop.gameloft.com/mtk_mc5/js/jquery.js"></script>
+        <script type="text/javascript" src="http://wapshop.gameloft.com/mtk_mc5/js/cookies.js"></script>
+        <script type="text/javascript" src="http://wapshop.gameloft.com/mtk_mc5/js/responsivePaginate.js"></script>
+        <script type="text/javascript" src="http://wapshop.gameloft.com/mtk_mc5/js/jcarousellite.1.0.1.pack.js"></script>
+                <script type="text/javascript" src="http://wapshop.gameloft.com/mtk_mc5/js/index_carousel.js"></script>
+                </script>
+    <link rel="canonical" href="http://wapshop.gameloft.com/mtk_mc5/"/>
+    <meta name="title" content="Gameloft Android Games. Download Top HD Games for Android." />
+    <meta name="description" content="Download Gameloft games for Android in HD. The highest-quality gaming experience for Android smartphones." />
+    <meta name="keywords" content="android games, download android games, gameloft android games, gameloft android, android game" />
+    
+    </head>
+    <body>
+        <div id="banner">
+    <a href="http://wapshop.gameloft.com/mtk_mc5/index.php?phoneId=1402&amp;c=73&amp;sv=128gqvdvknmfy8x6mujyggsgn" title="Gameloft HD Games for Android - Top Android Games">
+        <img src="http://wapshop.gameloft.com/mtk_mc5/images/gl_banner.gif" alt="Gameloft HD Games for Android - Top Android Games">
+    </a>
+</div>
+<div class="clear"></div><div id="nav">
+            <table>
+                <tr>
+                    <td style="width: 25%" class=""><a href="index.php?phoneId=1402&amp;sv=128gqvdvknmfy8x6mujyggsgn&amp;c=73" title="Home">Home</a></td><td style="width: 25%" class=""><a href="trailer.php?phoneId=1402&amp;sv=128gqvdvknmfy8x6mujyggsgn&amp;c=73" title="Trailer">Trailer</a></td><td style="width: 25%" class=""><a href="images.php?phoneId=1402&amp;sv=128gqvdvknmfy8x6mujyggsgn&amp;c=73" title="Images">Images</a></td><td style="width: 25%" class=""><a href="download_page.php?phoneId=1402&amp;sv=128gqvdvknmfy8x6mujyggsgn&amp;c=73" title="Download">Download</a></td>
+                </tr>
+            </table>
+        </div>
+        <div class="clear"></div><div class="carousel main" id="screenshot_container" style="width: 314px !important;"><div class="jCarouselLite" style="visibility: visible; overflow: hidden; position: relative; z-index: 2; left: 0px; width: 314px; height: auto;"><ul style="margin: 0pt; padding: 0pt; position: relative; list-style-type: none; z-index: 1; width: 2890px; left: -850px;"><li style="overflow: hidden; width: 314px; float: left; "><img width = "300px" id="img-1" src="http://wapshop.gameloft.com/mtk_mc5/images/1.jpg" /></li><li style="overflow: hidden; width: 314px; float: left; "><img width = "300px" id="img-2" src="http://wapshop.gameloft.com/mtk_mc5/images/2.jpg" /></li><li style="overflow: hidden; width: 314px; float: left; "><img width = "300px" id="img-3" src="http://wapshop.gameloft.com/mtk_mc5/images/3.jpg" /></li><li style="overflow: hidden; width: 314px; float: left; "><img width = "300px" id="img-4" src="http://wapshop.gameloft.com/mtk_mc5/images/4.jpg" /></li><li style="overflow: hidden; width: 314px; float: left; "><img width = "300px" id="img-5" src="http://wapshop.gameloft.com/mtk_mc5/images/5.jpg" /></li></ul></div><div class="prev"></div><div class="next"></div><div class="clear"></div></div><div class="delimiter_6"></div><div class="delimiter_6"></div><div class="delimiter_6"></div><div align="center">© 2014 Gameloft. All Rights Reserved. <br/>Gameloft, the Gameloft logo are trademarks of Gameloft in the US and/or other countries.</div>
+        <div class="footer"></div>
+                <script type="text/javascript" language="JavaScript">
+                $(function() {
+                    $(".carousel").jCarouselLite({
+                        btnNext: ".next",
+                        btnPrev: ".prev",
+                        visible: 1
+                    });
+                    $(".carousel2").jCarouselLite({
+                        btnNext: ".next2",
+                        btnPrev: ".prev2",
+                        visible: 2
+                    });
+                });
+
+                $(document).ready(function() {
+                    if (parseInt($(window).width()) > 400) {
+                        $("#carousel_cont .jCarouselLite").css("width","330px");
+                        $("#carousel_cont").css("width","430px");
+                    } else {
+                        $("#carousel_cont .jCarouselLite").css("width","220px");
+                        $("#carousel_cont").css("width","294px");
+                    }
+                });
+
+                $(window).resize(function() {
+                    if (parseInt($(window).width()) > 400) {
+                        $("#carousel_cont .jCarouselLite").css("width","330px");
+                        $("#carousel_cont").css("width","430px");
+                    } else {
+                        $("#carousel_cont .jCarouselLite").css("width","220px");
+                        $("#carousel_cont").css("width","294px");
+                    }
+                });
+
+                </script>
+                    <script type="text/javascript" src="http://wapshop.gameloft.com/mtk_mc5/js/carousel.js"></script>
+                    <script type="text/javascript">
+                        window.onload = function() {
+                            new Carousel({
+                            'container': 'carousel-container',
+                            'imagesContainer': 'carousel-images-container',
+                            'images': 'carousel-image',
+                            'leftArrow': 'carousel-left-arrow',
+                            'rightArrow': 'carousel-right-arrow'
+                    });
+                    
+                    var src = $('#img-splash').attr('src');
+                            function getImageSize(src)
+                            {
+                                    var newImg = new Image();
+                                    newImg.src = src;
+                                    var height = newImg.height;
+                                    return 209;
+                            }
+
+                            var currentHeight = getImageSize(src);
+                            //alert(currentHeight);
+                            $('#screenshot_container').find('li').css('height', currentHeight);
+                        }
+                    </script>
+    </body>
+</html>
---
+++ sitemap.xml
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Google Tag Manager -->
+        <script>(function (w, d, s, l, i) {
+                w[l] = w[l] || [];
+                w[l].push({'gtm.start':
+                            new Date().getTime(), event: 'gtm.js'});
+                var f = d.getElementsByTagName(s)[0],
+                        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                j.async = true;
+                j.src =
+                        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+                f.parentNode.insertBefore(j, f);
+            })(window, document, 'script', 'dataLayer', 'GTM-PP282DF');</script>
+        <!-- End Google Tag Manager -->
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0">
+        <link rel="preconnect" href="//fonts.googleapis.com">
+        <title>Ooops - Page not found</title>
+        <style>@import url(https://fonts.googleapis.com/css2?family=Roboto&display=swap);*{font-family:Roboto,sans-serif;font-size:16px;letter-spacing:.02em;font-style:normal;font-weight:500}.timer{font-size:1.25rem;font-weight:700;color:#1890ff}a{padding:1.5rem 2.5rem;border-radius:.75rem;background:#1890ff;text-decoration:none;color:#fff;text-transform:uppercase;font-weight:700;font-size:.875rem}</style>
+        <script type="text/javascript">
+            function pushDl() {
+                var queryString = window.location.search;
+                var urlParams = new URLSearchParams(queryString);
+                var categoryValue = '';
+                var labelValue = '';
+                if (urlParams.has('adid')) {
+                    categoryValue = 'ADID';
+                    labelValue = urlParams.get('adid');
+                }
+                if (urlParams.has('ADID')) {
+                    categoryValue = 'ADID';
+                    labelValue = urlParams.get('ADID');
+                }
+                if (urlParams.has('igagame')) {
+                    categoryValue = 'IGA';
+                    labelValue = urlParams.get('igagame');
+                }
+                if (urlParams.has('IGAGAME')) {
+                    categoryValue = 'IGA';
+                    labelValue = urlParams.get('IGAGAME');
+                }
+                dataLayer.push({
+                    'event': 'click_404',
+                    'category': categoryValue,
+                    'label': labelValue
+                })
+            }
+        </script> 
+    </head>
+    <body style="margin:0;padding:0">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PP282DF"
+                          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        <div style="width:100%;height:100%;text-align:center;margin: 9.5rem auto;line-height: 1.875rem;">
+            <div style="">
+                <img style="max-width: 80%;" src="https://media07-gl-ssl-gzip.gameloft.com/assets/404/ooops.png"/>
+            </div>
+            <div>
+                <span id="timer" class="timer"></span>
+            </div>
+            <div>
+                <p style="display: inline-grid;">
+                    <strong style="font-weight:bold">Oops! The requested page was not found.</strong>
+                    <span>We are redirecting you to our local shop.</span>
+                </p>
+            </div>
+            <div style="margin: 3.125rem 0;">
+                <a onclick="pushDl()" id="directLink" class="botton" href="https://crt.gameloft.com/?from404=1">TAKE ME THERE NOW</a>
+            </div>
+        </div>
+        <script>
+            baseUrl = 'https://crt.gameloft.com';
+            newPath = window.location.search ? baseUrl + window.location.search + '&from404=1' : baseUrl + '?from404=1';
+            document.getElementById("directLink").href = newPath;
+            var countDownDate = 5e3, now = 0, x = setInterval(function () {
+                var e = countDownDate - now;
+                now += 1e3;
+                var a = e / 1e3;
+                console.log(a), document.getElementById("timer").innerHTML = "00 : 0" + a + "s ", e <= 0 && (clearInterval(x), window.location.href = newPath)
+            }, 1e3);
+        </script>
+    </body>
+</html>
